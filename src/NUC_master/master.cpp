8c99e4103084765aa67922ae455cfa819105cc68
iO Ninja <luke.fisklennon@gmail.com>
2017-02-17 16:17:25 +1100

    Update master.cpp

--- src/NUC_master/master.cpp
+++ src/NUC_master/master.cpp
@@ -1,1 +1,14 @@
-#include "roborio_bridge"
+//#include "roborio_bridge.cpp"
+#include "image_processing.h"
+
+int main() {
+  image_setup();
+  image_set_mode(IMAGE_MODE_SHIP);
+  //image_set_mode(IMAGE_MODE_SHIP);
+  while(1) {
+    image_update();
+    printf("%f\n",image_get_distance());
+    printf("%f\n",image_get_x());
+  }
+  return 0;
+}
